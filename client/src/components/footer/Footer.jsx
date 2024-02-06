941efbc3658ed8870cea17fc0590028651c6052b
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-02-06 21:38:19 +0530

    Hotels Page

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 TravelEase.</div>
+      <div className="fText">Copyright © 2024 TravelEase.</div>
     </div>
   );
 };
